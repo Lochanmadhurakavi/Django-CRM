dccaf4a0a446decedfc592fe7772d45c2bbef834
Lochan <lochan.246@gmail.com>
2023-03-17 08:53:51 +0530

    Final Project

--- website/forms.py
+++ website/forms.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Record
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
     first_name = forms.CharField(label="",max_length=100 ,widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
-    last_name = forms.EmailField(label="",max_length=100 ,widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}))
+    last_name = forms.CharField(label="",max_length=100 ,widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}))
 
     class Meta:
         model = User
@@ -33,3 +34,17 @@ class SignUpForm(UserCreationForm):
 
 
 
+# create add record form
+class AddRecordForm(forms.ModelForm):
+    first_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder':'First Name', 'class':'form-control'}), label="")
+    last_name = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder':'Last Name', 'class':'form-control'}), label='')
+    email = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder':'Email', 'class':'form-control'}), label='')
+    phone = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder':'Phone', 'class':'form-control'}), label='')
+    address = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder':'Address', 'class':'form-control'}), label='')
+    city = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder':'City', 'class':'form-control'}), label='')
+    state = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder':'State', 'class':'form-control'}), label='')
+    zipcode = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder':'Zipcode', 'class':'form-control'}), label='')
+
+    class Meta:
+        model = Record
+        exclude = ('user',)
